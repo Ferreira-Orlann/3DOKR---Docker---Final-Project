e5f9c8372bc43a6dfe6f0e8dbf7048202e6b9069
Ferreira-Orlann <118252904+Ferreira-Orlann@users.noreply.github.com>
2025-01-25 17:03:59 +0100

    Diagram

--- diagram/diagram.py
+++ diagram/diagram.py
@@ -1,6 +1,17 @@
 from diagrams import Cluster, Diagram
-from diagrams.onprem.
+from diagrams.onprem.database import PostgreSQL
+from diagrams.programming.language import NodeJS, Python, Csharp
+from diagrams.onprem.inmemory import Redis
 
 with Diagram("Project", show=False):
     with Cluster("Swarm Cluster") as cluster:
-        Docker("Manager") >> Docker("Worker 1") >> Docker("Worder 2")
+        
+        with Cluster("Web Network"):
+            nodejs = NodeJS("Result App")
+            vote = Python("Vote App")
+        with Cluster("Internal Network"):
+            vote = Csharp("Worker App")
+            db = PostgreSQL("Database")
+            cache = Redis("Redis")
+        vote >> db
+        vote >> cache
